34d956b28e9ccbf5403559a4211f9ed4d4f0c871
Kenneth Maguire <kenneth.maguire@ucdenver.edu>
2018-10-30 20:43:16 -0600

    have number of heroes and comic completed

---
+++ Spiderman.py
@@ -0,0 +1,24 @@
+import csv
+import collections as collect
+
+
+
+heroDict = collect.OrderedDict()
+comicDict = collect.OrderedDict()
+#https://docs.python.org/3.7/library/csv.html
+with open('nodes.csv', 'r') as nodefile:
+    next(nodefile)
+    nodeReader = csv.reader(nodefile, skipinitialspace=True, delimiter=',', quotechar='"')
+    for row in nodeReader:
+        if row[1] == 'comic':
+            comicDict[row[0]] = row[1]
+        elif row[1] == 'hero':
+            heroDict[row[0]] = row[1]
+
+print(nodeReader)
+print(comicDict)
+print(heroDict)
+print("The number of heroes is: ")
+print(len(heroDict))
+print("The number of heroes is: ")
+print(len(comicDict))
