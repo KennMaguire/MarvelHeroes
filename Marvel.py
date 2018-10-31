9e68d1bfb4618b2ff709bf6dfd58158f1c21c154
Kenneth Maguire <kenneth.maguire@ucdenver.edu>
2018-10-31 12:52:01 -0600

    finished heroes per comic

--- Marvel.py
+++ Marvel.py
@@ -3,22 +3,46 @@ import collections as collect
 
 
 
-heroDict = collect.OrderedDict()
-comicDict = collect.OrderedDict()
+nodeHeroDict = {}#collect.OrderedDict()
+nodeComicDict = {}#collect.OrderedDict()
 #https://docs.python.org/3.7/library/csv.html
 with open('nodes.csv', 'r') as nodefile:
     next(nodefile)
     nodeReader = csv.reader(nodefile, skipinitialspace=True, delimiter=',', quotechar='"')
     for row in nodeReader:
         if row[1] == 'comic':
-            comicDict[row[0]] = row[1]
+            nodeComicDict[row[0]] = row[1]
         elif row[1] == 'hero':
-            heroDict[row[0]] = row[1]
+            nodeHeroDict[row[0]] = row[1]
 
 print(nodeReader)
-print(comicDict)
-print(heroDict)
+print(nodeComicDict)
+print(nodeHeroDict)
 print("The number of heroes is: ")
-print(len(heroDict))
+print(len(nodeHeroDict))
 print("The number of comics is: ")
-print(len(comicDict))
+print(len(nodeComicDict))
+
+
+edgeHeroesComicDict = collect.defaultdict(list)#collect.OrderedDict()
+
+total = 0
+with open('edges.csv', 'r') as edgefile:
+    next(edgefile)
+    edgeReader = csv.reader(edgefile, skipinitialspace=True, delimiter=',', quotechar='"')
+    for row in edgeReader:
+        edgeHeroesComicDict[row[1]].append(row[0])
+    for k,v in edgeHeroesComicDict.items():
+        print(v)
+        total += len(v)
+
+
+
+aveHperC = (total/len(edgeHeroesComicDict))
+
+
+
+print(edgeHeroesComicDict)
+print(len(edgeHeroesComicDict))
+print("The average number of heroes per comic is: ")
+print(aveHperC)
